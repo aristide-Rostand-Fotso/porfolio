15fd044af693656d586e039e56e6754e58284455
Aristide Fotso <aristidefotso4@gmail.com>
2026-08-27 19:45:19 +0100

    package EntityScan et Database dialect

--- src/main/java/com/aristide/porfolio/PorfolioApplication.java
+++ src/main/java/com/aristide/porfolio/PorfolioApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+@EntityScan("com.aristide.porfolio.Model")
+@EnableJpaRepositories("com.aristide.porfolio.Repository")
 @SpringBootApplication
 @EnableAsync
 public class PorfolioApplication {
